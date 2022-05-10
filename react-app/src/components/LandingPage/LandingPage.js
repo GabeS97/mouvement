dddc02c4fbf16a7b76c04c921c49882c926773d4
Gabriel Sitorus <sitorusgg@gmail.com>
2022-05-10 12:31:45 +0700

    added a couple stuff on the frontend, not too much, finish majoirty tomorrow

---
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LandingPage.css'
+const LandingPage = () => {
+    return (
+        <div className='landingPage'>
+            <div className="landingPage__contents">
+            landingPage
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
