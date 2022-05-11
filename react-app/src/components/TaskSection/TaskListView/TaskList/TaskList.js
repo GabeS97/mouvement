ae6cb6a4a10315e760244ae1520e6cdb2a5cc21f
Gabriel Sitorus <sitorusgg@gmail.com>
2022-05-12 05:03:48 +0700

    tasklist made, but still need to be edited

--- react-app/src/components/TaskSection/TaskListView/TaskList/TaskList.js
+++ react-app/src/components/TaskSection/TaskListView/TaskList/TaskList.js
@@ -4,6 +4,38 @@ import './TaskList.css'
 const TaskList = ({ boards }) => {
     const { boardId } = useParams()
     const board = boards.find(board => board.id === +boardId)
+
+    const items = document.querySelectorAll('.taskList__item')
+    const columns = document.querySelectorAll('.taskList__column')
+
+    console.log(items)
+    items.forEach(item => {
+        item.addEventListener('dragstart', dragStart)
+        item.addEventListener('dragend', dragEnd)
+    })
+    let dragItem = null
+
+    function dragStart() {
+        dragItem = this;
+        setTimeout(() => this.className = 'invisible', 0)
+    }
+    function dragEnd() {
+        this.className = 'taskList__item'
+        dragItem = null;
+    }
+
+    columns.forEach(column => {
+        column.addEventListener('dragover', dragOver);
+        column.addEventListener('drop', dragDrop);
+    });
+
+    function dragOver(e) {
+        e.preventDefault()
+    }
+
+    function dragDrop() {
+        this.append(dragItem)
+    }
     return (
         <div className="taskList">
             <div className="taskList__contents">
@@ -18,8 +50,23 @@ const TaskList = ({ boards }) => {
                     </div>
                 </div>
 
-                <table className="taskList__table">
-                </table>
+                <div className="taskList__container">
+                    <div className="taskList__column">
+                        <h1>To Do</h1>
+                        <div className="taskList__item" draggable="true">Finish TaskList view</div>
+                        <div className="taskList__item" draggable="true">Finish PUT frontend for Boards</div>
+                        <div className="taskList__item" draggable="true">Fix remaining bugs on first feat</div>
+                        <div className="taskList__item" draggable="true">Change to state value later</div>
+                    </div>
+
+                    <div className="taskList__column">
+                        <h1>Doing</h1>
+                    </div>
+
+                    <div className="taskList__column">
+                        <h1>Done</h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
