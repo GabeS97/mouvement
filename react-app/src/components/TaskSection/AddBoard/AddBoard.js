2f415a9ef992d4f1aea264e45461027ee26cd29e
Gabriel Sitorus <sitorusgg@gmail.com>
2022-05-11 12:06:42 +0700

    contineu form

--- react-app/src/components/TaskSection/AddBoard/AddBoard.js
+++ react-app/src/components/TaskSection/AddBoard/AddBoard.js
@@ -10,6 +10,22 @@ const AddBoard = () => {
                     />
                 </div>
 
+                <div className="addBoard__descriptiom">
+                    <input className='addBoard__boardDescription'
+                        placeholder='Add a description'
+                    />
+                </div>
+
+                <div className="addBoard__custom__select">
+                    <select>
+                        <option disabled>Templates</option>
+                        <option>Quick Note</option>
+                        <option>Task List</option>
+                        <option>Reading List</option>
+                        <option>Journal</option>
+                        <option>Personal Home</option>
+                    </select>
+                </div>
             </form>
         </div>
     )
